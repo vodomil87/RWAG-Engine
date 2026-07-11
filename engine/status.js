cb1a2870e0190703cab2d2f64ea045497c69c2bd
vodomil87 <vodomil87@gmail.com>
2026-07-11 11:29:29 +0200

    Update status.js

--- engine/status.js
+++ engine/status.js
@@ -1,1 +1,43 @@
-;
+const Status = {
+
+    render() {
+
+        const bar = document.getElementById("status");
+
+        if (!bar) return;
+
+        let rep = Engine.state.reputation;
+
+        let icon;
+
+        if (rep > 0) {
+
+            icon = icons.rep_pos;
+
+        } else if (rep < 0) {
+
+            icon = icons.rep_neg;
+
+        } else {
+
+            icon = icons.rep_neu;
+
+        }
+
+        let text = rep > 0
+            ? `+${rep}`
+            : `${rep}`;
+
+        bar.innerHTML = `
+            <button id="menuButton">
+                ${icons.menu}
+            </button>
+
+            <div class="status-right">
+                ${icon} ${text}
+            </div>
+        `;
+
+    }
+
+};
