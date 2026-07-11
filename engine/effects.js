32937534ac22a072dee010c7fc397aeb3cf84ec2
vodomil87 <vodomil87@gmail.com>
2026-07-11 21:56:14 +0200

    Update effects.js

--- engine/effects.js
+++ engine/effects.js
@@ -1,1 +1,23 @@
-Effects.apply(scene.effects);
+const Effects = {
+
+    apply(effects) {
+
+        if (!effects) return;
+
+        effects.forEach(effect => {
+
+            switch (effect.type) {
+
+                case "reputation":
+
+                    Engine.state.reputation += effect.value;
+
+                    break;
+
+            }
+
+        });
+
+    }
+
+};
