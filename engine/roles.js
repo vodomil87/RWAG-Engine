e8d223a42bb252a4866b935ac344d7eaf3da6dc8
vodomil87 <vodomil87@gmail.com>
2026-07-14 14:06:07 +0200

    Update roles.js

--- engine/roles.js
+++ engine/roles.js
@@ -1,1 +1,50 @@
-;
+const Roles = {
+
+    async load(roleSet){
+        console.log(
+            "LOAD ROLES:",
+            roleSet
+        );
+
+        const response = await fetch(
+            `roles/${roleSet}.json`
+        );
+
+        if(!response.ok){
+            throw new Error(
+                "Nelze načíst role: " + roleSet
+            );
+        }
+
+        const data =
+            await response.json();
+
+        console.log(
+            "ROLES LOADED:",
+            data
+        );
+
+        return data.roles;
+    },
+
+    assign(roles, count=1){
+
+        console.log(
+            "ASSIGN ROLES",
+            count
+        );
+
+        const shuffled =
+            [...roles]
+            .sort(
+                ()=>Math.random()-0.5
+            );
+
+        return shuffled.slice(
+            0,
+            count
+        );
+    }
+};
+
+window.Roles=Roles;
