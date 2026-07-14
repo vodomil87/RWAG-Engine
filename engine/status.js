b1e70249bf90675a4257bc6d3205ba49c1ca8abc
vodomil87 <vodomil87@gmail.com>
2026-07-14 16:01:54 +0200

    Update status.js

--- engine/status.js
+++ engine/status.js
@@ -2,6 +2,13 @@ const Status={
 render(){
  const bar=document.getElementById("status");
  if(!bar) return;
+ if(!Engine.game){
+    bar.innerHTML = `
+        <button id="menuButton">${icons.menu}</button>
+    `;
+    Menu.init();
+    return;
+}
  const r=Engine.state.reputation;
  const icon=r>0?icons.rep_pos:r<0?icons.rep_neg:icons.rep_neu;
  bar.innerHTML=`
