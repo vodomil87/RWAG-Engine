cd80fbb6a51fe29baabb7eb821b1a8a6c7a721c9
vodomil87 <vodomil87@gmail.com>
2026-07-15 11:04:05 +0200

    Update settings.js

--- engine/settings.js
+++ engine/settings.js
@@ -1,28 +1,24 @@
 const Settings = {
+
+    init(){
+        this.load();
+    },
+    
     setTheme(theme){
-        document.body.className = "theme-" + theme;
+        document.body.className =
+            "theme-" + theme;
         localStorage.setItem(
             "rwag_theme",
-            theme
-        );
-        console.log(
-            "THEME SET:",
             theme
         );
     },
 
     load(){
         const theme =
-            localStorage.getItem(
-                "rwag_theme"
-            )
+            localStorage.getItem("rwag_theme")
             || "dark";
         document.body.className =
             "theme-" + theme;
-        console.log(
-            "THEME LOADED:",
-            theme
-        );
     }
 };
 
