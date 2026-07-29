48c444126a72b6076d80af4ab06750eb0604155b
vodomil87 <vodomil87@gmail.com>
2026-07-29 10:44:33 +0200

    Update status.js

--- engine/status.js
+++ engine/status.js
@@ -4,7 +4,7 @@ render(){
  if(!bar) return;
  if(!Engine.game){
     bar.innerHTML = `
-        <button id="menuButton">${icons.menu}</button>
+        <button id="menuButton">${icons.menu} Menu</button>
     `;
     Menu.init();
     return;
@@ -13,7 +13,7 @@ render(){
  const icon=r>0?icons.rep_pos:r<0?icons.rep_neg:icons.rep_neu;
  bar.innerHTML=`
  
-<button id="menuButton">${icons.menu}</button>
+<button id="menuButton">${icons.menu} Menu</button>
 
 <span>
 ${icon} ${r>0?"+":""}${r}
